135ad90cc1dc45c81e0da47499c46f3e26dd9f3e
elijhac <ej9314@hotmail.com>
2022-11-19 23:52:24 -0500

    Lab completed

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,6 +1,10 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
 
 import java.util.Date;
 
@@ -9,12 +13,28 @@ import java.util.Date;
  */
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
-
     @Test
     public void createDogTest(){
-       String name = "Wesley";
-       Date birthDate = new Date(21, 5, 3);
+       String name = "Scooby";
+       Date birthDate = new Date();
+       Dog dog = AnimalFactory.createDog(name, birthDate);
+
+       String expected = "Scooby";
+       String actual = dog.getName();
+
+        Assert.assertEquals(expected, actual);
     }
 
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+    @Test
+    public void createCatTest(){
+        String name = "Tom";
+        Date birthDate = new Date();
+        Cat cat = AnimalFactory.createCat(name, birthDate);
+
+        String expected = "Tom";
+        String actual = cat.getName();
+
+        Assert.assertEquals(expected, actual);
+    }
 }
